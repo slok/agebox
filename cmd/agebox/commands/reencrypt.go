7ce5752a4c2ed25502b5990b53dd44b11c17e65f
Xabier Larrakoetxea <me@slok.dev>
2021-03-11 11:39:36 +0100

    Add update alias for reencrypt command
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- cmd/agebox/commands/reencrypt.go
+++ cmd/agebox/commands/reencrypt.go
@@ -25,6 +25,7 @@ func NewReencryptCommand(app *kingpin.Application) Command {
 	c := &reencryptCommand{}
 	cmd := app.Command("reencrypt", "Reencrypts the tracked files.")
 	cmd.Alias("recrypt")
+	cmd.Alias("update")
 	cmd.Flag("public-keys", "Path to public keys.").Default("keys").Short('p').StringVar(&c.PubKeysPath)
 	cmd.Flag("private-key", "Path to private key.").Required().Short('i').StringVar(&c.PrivateKeyPath)
 	cmd.Flag("dry-run", "Enables dry run mode, write operations will be ignored").BoolVar(&c.DryRun)
